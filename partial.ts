6019420ee0f90df3ff2efd9857c0870153ef9258
Amit Maurya <amitmaurya700@gmail.com>
2026-06-09 17:18:03 +0530

    Update partial.ts

--- partial.ts
+++ partial.ts
@@ -10,6 +10,7 @@ interface UserProfile  {
    email: string,
  }
 
+//created and function and passsed the email as new interface
 function addUserProfile(userProfile: UserProfile, updateUser: UpdateUser){
   return {...userProfile, ...updateUser};
 }
@@ -21,6 +22,7 @@ const profileOne: UserProfile = {
   address: "345 street",
 }
 
+// passing the params which is require here
 const profileSummary = addUserProfile(profileOne, {email: "newtestOne@test.com"});
 console.log(profileSummary);
 
